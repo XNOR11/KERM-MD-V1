76dd1d7d8720cd92a663b3e6a5163dc9b79d187b
X.N.O.R Education <adithyashavinda2005@gmail.com>
2025-03-13 08:54:22 +0530

    Update config.js

--- config.js
+++ config.js
@@ -19,7 +19,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "PUT YOUR SESSION ID HERE",
+SESSION_ID: process.env.SESSION_ID || "KERM-MD-V1~qR5TlbCL#6ZULbb4f_Awcg_vx1k66GCeZv8JGUYhEnw5sw0wXpPc",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "private",
 AUTO_VOICE: process.env.AUTO_VOICE || "false",
